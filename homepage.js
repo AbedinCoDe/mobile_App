d793b3329ae134e7ba2abf95edcb8bd69da6a7f3
AbedinCoDe <kausarulabedin2010@yahoo.com>
2025-03-02 16:12:00 +0600

    Cash Out Section Complete

--- homepage.js
+++ homepage.js
@@ -1,3 +1,8 @@
+document.getElementById('log_Out').addEventListener('click', function(){
+    window.location.href = ('/index.html');
+})
+
+
 
 
 document.getElementById('add_Money').addEventListener('click', function(){
@@ -6,15 +11,16 @@ document.getElementById('add_Money').addEventListener('click', function(){
 })
 
 
+let displayAmount = parseInt(document.getElementById('display_Amount').innerText);
+
+
+
 document.getElementById('for_Add_Money_Button').addEventListener('click', function(){
 
     const amountToAdd = parseInt(document.getElementById('amount_To_Add').value);
     const pinNumber = document.getElementById('pin_Number').value;
 
-    console.log(amountToAdd, pinNumber);
-    let displayAmount = parseInt(document.getElementById('display_Amount').innerText);
-
-    console.log(isNaN(amountToAdd));
+    
 
     if(pinNumber === '1234'){
         displayAmount += amountToAdd;
@@ -24,6 +30,24 @@ document.getElementById('for_Add_Money_Button').addEventListener('click', functi
 
     document.getElementById('display_Amount').innerText = displayAmount;
     
+})
 
+
+document.getElementById('withdraw_Money').addEventListener('click', function(){
+    const displayCashOut = document.getElementById('for_Cash_Out');
+    displayCashOut.classList.remove('hidden');
+})
+
+
+document.getElementById('for_Withdraw_Button').addEventListener('click', function(){
+    const amountToWithdraw = parseInt(document.getElementById('amount_To_Withdraw').value);
+    const PinNumber = document.getElementById('withdraw_pin_Number').value;
+
+    if(PinNumber === '1234'){
+        displayAmount -= amountToWithdraw
+    }
+
+    document.getElementById('display_Amount').innerText = displayAmount;
 
 })
+
